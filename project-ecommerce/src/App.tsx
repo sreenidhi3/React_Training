2d742a905a19215839f90de324396fca397cdfcd
Sreenidhi Bendre <sreenidhi.bendre@gmail.com>
2022-08-17 11:06:54 +0530

    Add: Setup basic routing for app

--- project-ecommerce/src/App.tsx
+++ project-ecommerce/src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Link, Route, Routes, useNavigate, useParams } from 'react-router-dom';
 import './App.css';
+
+const Login =()=>{
+  return(
+    <div>
+     <h2>Login</h2>
+    </div>
+  )
+}
+
+const Products =()=>{
+  return(
+    <div>
+     <h2>Products</h2>
+    </div>
+  )
+}
+
+const SignUp =()=>{
+  return(
+    <div>
+     <h2>SignUp</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
   );
 }
+
 
 export default App;
