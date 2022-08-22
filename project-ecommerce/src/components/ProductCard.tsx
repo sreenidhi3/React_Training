a48359ab92dcf2191b536dba727ad3289d8aa245
Sreenidhi Bendre <sreenidhi.bendre@gmail.com>
2022-08-22 14:53:19 +0530

    Add: product component fot catalogue and individual product details page

--- project-ecommerce/src/components/ProductCard.tsx
+++ project-ecommerce/src/components/ProductCard.tsx
@@ -1,25 +1,49 @@
-import { FC, useState } from "react"
+import { FC, useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import { addToCartActionA, removeFromCartAction, removeFromCartActionA } from "../actions/products.actions"
+import { RootState } from "../store"
 import "../styles/products.styles.css"
 import { ProductType } from "../types/products.types"
 
 const ProductCard:FC<ProductType>=(prod:ProductType)=>{
-
+    const {cart} = useSelector((state: RootState) => state.productReducer);
+    const dispatch =  useDispatch();
     const [added, setAdded] = useState<boolean>(false)
-    const handleCart=()=>{
+    useEffect(()=>{
+        let add = cart.filter((it)=>it.id===prod.id).length
+        add ? setAdded(true) : setAdded(false)
+    },[])
+    const handleCart=(id:number)=>{
+        if(added){
+            dispatch(removeFromCartActionA(id))
+        }else{
+            dispatch(addToCartActionA(id))
+        }
         setAdded(!added)
+
     }
+    // console.log("cart: ", cart)
     
     return(
-        <div className="product-container">
-                <div className="row center"><img className="img" src={prod.image} alt={prod.title}/></div>
+        
+            <div className="product-container">
+                <Link to={prod.id.toString()}>
+                    <div className="row center">
+                        <img className="img" src={prod.image} alt={prod.title}/>
+                    </div>
+                </Link>
                 <div>
-                    <h3> {prod.title} </h3> 
+                    <Link to={prod.id.toString()}>
+                        <h3> {prod.title} </h3> 
+                    </Link>
                     {/* <p> {prod.description.slice(0,100)}... </p> */}
                     <h4> US ${prod.price}</h4>
-                    <button onClick={handleCart} className={added ?"btn-added-to-cart":"btn-primary-full"}>{added ? "Remove From Cart" : "Add To Cart"}</button>
+                    <button onClick={()=>handleCart(prod.id)} className={added ?"btn-added-to-cart":"btn-primary-full"}>{added ? "Remove From Cart" : "Add To Cart"}</button>
                 </div>
                 {/* title */}
-        </div>
+            </div>
+       
     )
 }
 
