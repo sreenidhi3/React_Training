83291f0aeb6978ad4d9b3b857abba0994dbd17a3
Sreenidhi Bendre <sreenidhi.bendre@gmail.com>
2022-08-30 14:48:42 +0530

    Add: Test cases for Error Page rendering

--- project-ecommerce/src/__tests__/navigation.tests.tsx
+++ project-ecommerce/src/__tests__/navigation.tests.tsx
@@ -2,6 +2,7 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "../App";
+import Error from "../pages/Error";
 import configureStore from 'redux-mock-store';
 
 
@@ -88,12 +89,19 @@ describe('Logged In User', () => {
         expect(screen.getByText("Filter by Category:")).toBeInTheDocument(); ;
     })
 
+    test("must render single product details page on click product card",()=>{
+        expect(screen.getByText("Products")).toBeInTheDocument()
+        fireEvent.click(screen.getByText("Products"))
+        expect(screen.getByText(/Fjallraven/i)).toBeInTheDocument()
+        fireEvent.click(screen.getByText(/Fjallraven/i))
+        expect(screen.getByText(/Perfect/i)).toBeInTheDocument()
+    })
+
     test("must render cart page",()=>{
         expect(screen.getByText("Logout")).toBeInTheDocument()
         fireEvent.click(screen.getByText("Cart"))
         expect(screen.getByText("Your Store Cart is empty")).toBeInTheDocument(); ;
     })
-
   });
 
 
@@ -169,3 +177,70 @@ describe('Logged In User', () => {
         expect(screen.getByText("Sign Up")).toBeInTheDocument()
     })
   });
+
+
+describe("check error page render", ()=>{
+    let store;
+    beforeEach(()=>{
+        store = mockStore({
+            loginReducer: {
+                isUserLoggedIn: false,
+                user: {
+                    name: "",
+                    email: "",
+                    token: ""
+                },
+                error: ""
+            },
+            productReducer:{
+                activeCategory:"",
+                allCategories:['electronics', 'jewelery', "men's clothing", "women's clothing"],
+                allProducts:[{
+                    id :1,
+                    title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+                    price:109.95,
+                    description:"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+                    category:"men's clothing",
+                    image:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                    rating:{"rate":3.9,"count":120}
+                }, {
+                    "id": 7,
+                    "title": "White Gold Plated Princess",
+                    "price": 9.99,
+                    "description": "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
+                    "category": "jewelery",
+                    "image": "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+                    "rating": {
+                      "rate": 3,
+                      "count": 400
+                    }
+                  }],
+                activeProducts:[],
+                cart:[]
+            },
+            signUpReducer:{
+                isUserSignedUp:false,
+                user:{
+                    id:null,
+                    email:"",
+                    token:""
+                },
+                error:""
+            }
+          });
+        render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Error/>
+                </BrowserRouter>
+            </Provider>
+            );
+    })
+    test("must render 404 page",()=>{
+        expect(screen.getByText(/Oops!/i)).toBeInTheDocument()
+    })
+    test("must render home page on click 'Go back to Home'",async()=>{
+        const linkElem = screen.getByRole('link', { name: "Home" })
+        expect(linkElem).toHaveAttribute('href', '/')
+    })
+})
